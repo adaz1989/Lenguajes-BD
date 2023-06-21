e0dfa85c2afad1aae8f0c373d0cf54eee81d1cf1
ItisYeWolf17 <105758299+ItisYeWolf17@users.noreply.github.com>
2023-06-21 14:01:34 -0600

    Proyecto Base
    Se agrega el formato de interfaz, interfaz login, falta programar eventos

---
+++ SisFactArrocan_LBD/src/main/java/com/g8lbd/sisfactarrocan_lbd/SisFactArrocan_LBD.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.g8lbd.sisfactarrocan_lbd;
+
+import Interfaz.Login;
+
+/**
+ *
+ * @author gblan
+ */
+public class SisFactArrocan_LBD {
+
+    public static void main(String[] args) {
+        Login log = new Login();
+        log.setVisible(true);
+    }
+}
